2fe6f08702dddd851c52a9d74af0727ce8cce4b6
Marco Tusa <tusamarco@gmail.com>
2024-09-27 16:14:30 +0200

    bug fix

--- sysbench_graph_creator.go
+++ sysbench_graph_creator.go
@@ -27,7 +27,7 @@ func main() {
 	help.Init()
 
 	//return version adn exit
-	if len(os.Args) <= 1 &&
+	if len(os.Args) == 1 &&
 		os.Args[1] == "--version" {
 		fmt.Println("Sysbench graph Creator version: ", version)
 		exitWithCode(0)
